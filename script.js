b0e5bbf0941c8dd5d39885247cde3bda4209a1ac
Wasookhan <wu028242@gmail.com>
2024-03-05 04:34:55 -0800

    commited

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const input = document.querySelector('#input');
-const btn = document.querySelector('#submit');
-let container = document.querySelector('.container');
+const input = document.querySelector("#input");
+const btn = document.querySelector("#submit");
+let container = document.querySelector(".container");
 
-btn.addEventListener('click',(event)=>{
+btn.addEventListener("click", (event) => {
   event.preventDefault();
-  if(input.value == "" && !input.value.includes('@') && !input.value.includes('.com')){
-   let error =  document.querySelector('#error').style.display = "block"
-  }else{
-error.style.display = "none"
-container.style.display = "none";
-let successMessage = document.querySelector('.success');
-successMessage.style.display = "block";
-let cancelbtn = document.querySelector('#cancel');
-cancelbtn.addEventListener('click',()=>{
-successMessage.style.display = "none";
-  container.style.display = "block";
-  container.style.display = "flex"
-  input.value = ""
-})
-  }
- 
+  const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
 
-})
+  if (!emailRegex.test(input.value)) {
+    let error = document.querySelector("#error").style.display = "block";
+  } else {
+    error.style.display = "none";
+    container.style.display = "none";
+    let successMessage = document.querySelector(".success");
+    successMessage.style.display = "block";
+    let email = document.querySelector('#email');
+    email.innerHTML = input.value;
+    let cancelbtn = document.querySelector("#cancel");
+    cancelbtn.addEventListener("click", () => {
+      successMessage.style.display = "none";
+      container.style.display = "block";
+      container.style.display = "flex";
+      input.value = "";
+     
+    });
+  }
+});
